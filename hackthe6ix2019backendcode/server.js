f2d7a3355ac36ef9c77af3920ad0607dfa4b82a5
Rolf Li <rolf.x.li@gmail.com>
2019-08-24 15:33:01 -0400

    working server

--- hackthe6ix2019backendcode/server.js
+++ hackthe6ix2019backendcode/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
+import Animal from './models/Animal';
+
 const app = express();
 
 
@@ -11,13 +13,26 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 
+mongoose.connect('mongodb://localhost:27017/animals');
+
 const connection = mongoose.connection;
 
 connection.once('open', () => {
     console.log('Connection established with MongoDB');
 });
 
+router.route('/Animals').get((req, res) => {
+    Animal.find((err, animals) => {
+        if (err) {
+            console.log('Error occured');
+        }
+        else {
+            res.json(animals);
+        }
+    })
+});
 
+app.use('/', router);
 
 app.listen(4000, () => {
     console.log('Express server is running on port 4000.')
